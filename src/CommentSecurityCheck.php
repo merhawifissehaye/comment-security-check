d7912639022b9647ba96ddf892cf2f600abb7013
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2017-01-30 09:06:50 +0300

    focusing spam words

--- src/CommentSecurityCheck.php
+++ src/CommentSecurityCheck.php
@@ -41,13 +41,13 @@ class CommentSecurityCheck {
     public function getSpamWords()
     {
         $words = array();
-        $sql = "SELECT spam_word from $this->tableName";
+        $sql = "SELECT id, spam_word from $this->tableName";
         $result = mysqli_query($this->link, $sql);
         if($result != null) {
             $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
             if($rows != false) {
                 foreach($rows as $row) {
-                    $words[] = $row['spam_word'];
+                    $words[$row['id']] = $row['spam_word'];
                 }
                 return $words;
             }
@@ -58,19 +58,33 @@ class CommentSecurityCheck {
     public function addToSpam($word)
     {
         if(!$this->isWordSpam($word)) {
-            if(!is_numeric($word)) {
-                $word = "'" . mysqli_real_escape_string($this->link, $word) . "'";
-            }
-            $sql = "INSERT INTO $this->tableName (spam_word) VALUES ($word)";
+            $word = $this->quote($word);
+            $sql = "INSERT INTO $this->tableName (id, spam_word) VALUES (NULL, $word)";
             mysqli_query($this->link, $sql);
         }
     }
 
+    public function removeFromSpam($id)
+    {
+        $sql = "DELETE FROM $this->tableName WHERE id = $id";
+        mysqli_query($this->link, $sql);
+    }
+
+    private function quote($word)
+    {
+        if(!is_numeric($word)) {
+            return "'" . mysqli_real_escape_string($this->link, $word) . "'";
+        }
+        return $word;
+    }
+
     public function isWordSpam($word)
     {
-        $sql = "SELECT spam_word from $this->tableName WHERE spam_word = $word";
+        $sql = "SELECT * from $this->tableName WHERE spam_word = $word";
         $result = mysqli_query($this->link, $sql);
-        if($result !== null && mysqli_num_rows($result)) return true;
+        if($result != false && $result != null) {
+            return mysqli_num_rows($result) > 0;
+        }
         return false;
     }
 
@@ -79,5 +93,29 @@ class CommentSecurityCheck {
         $this->link->close();
     }
 
+    function focusSpamWord($text) {
+        $spamWords = $this->getSpamWords();
+    // $replacedSpamWord = "";
+        foreach($spamWords as $spamWord) {
+            // if(!strstr($replacedSpamWord, $spamWord)) {
+                // $modifiedComment = preg_replace('/(' . $spamWord . ')/', '<strong><u>$1</u></strong>', $comment->comment);
+                // if($modifiedComment != $comment->comment) {
+                    // $replacedSpamWord = $spamWord;
+                    // $comment->comment = $modifiedComment;
+                // }
+            // }
+            $text = preg_replace('/(' . $spamWord . ')/i', '<strong><u>$1</u></strong>', $text);
+        }
+        return $text;
+    }
 
+    public function hasSpamWord($text) {
+        $spamWords = $this->getSpamWords();
+        foreach($spamWords as $spamWord) {
+            if(strstr(strtolower($text), strtolower($spamWord))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
